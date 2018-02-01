d8437f68e62242fd6b3ecff7870e0677acc73b93
Ing.Engelbert Zamudio Ponzzi <engel_zamudio@icloud.com>
2018-02-01 15:52:23 -0600

    Section 3-42 Installing Redux and Creating Basic Setup

---
+++ src/store/reducers/places.js
@@ -0,0 +1,16 @@
+const initialState =  {
+  places: [],
+  selectedPlace: null
+}
+
+const reducers = (state = initialState , action) => {
+  switch (action.type) {
+    // Hay que configurar nuestras acciones para configurarlas en nuestro selector medio de sus action.type
+    case value:
+
+    default:
+      return state
+  }
+}
+
+export default reducers
