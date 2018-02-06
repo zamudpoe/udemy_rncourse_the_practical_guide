72ad0338789cc6d5e95cab77b5cd7d8d1ac002a4
Ing.Engelbert Zamudio Ponzzi <engel_zamudio@icloud.com>
2018-02-06 09:53:32 -0600

    Section 4 - 53 Debuging Redux

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,20 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, compose } from 'redux'
 import placesReducer from './reducers/places'
 
 const rootReducer = combineReducers({
-    places: placesReducer
+  places: placesReducer
 })
 
+
+let composeEnhancers = compose
+
+// Solo si estamos en modo desarrollo
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+}
+
 const configureStore = () => {
-    return createStore(rootReducer)
+  return createStore(rootReducer, composeEnhancers())
 }
 
 export default configureStore
